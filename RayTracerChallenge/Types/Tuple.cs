d3fc24131bedac3c301f5a5853f9d97594a9b8f4
Richard <richard@sor.nz>
2019-09-12 20:20:44 +1200

    Add project files.

---
+++ RayTracerChallenge/Types/Tuple.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace RayTracerChallenge.Types
+{
+	public class Tuple : IEquatable<Tuple>
+	{
+		private static float Epsilon = 0.00001f;
+		public float X { get; set; }
+		public float Y { get; set; }
+		public float Z { get; set; }
+		
+		/// <summary>
+		/// W Indicates whenever a Tuple is a point or a vector
+		/// If W is 1, then this tuple is a POINT
+		/// If W is 0, then this tuple is a VECTOR
+		/// </summary>
+		public float W { get; private set; }
+
+		public bool IsPoint => Math.Abs(W - 1.0f) < Epsilon;
+		public bool IsVector => Math.Abs(W) < Epsilon;
+
+		public bool IsUnitVector => Math.Abs(Magnitude() - 1.0f) < Epsilon;
+
+
+
+		public Tuple(float x, float y, float z, float w)
+		{
+			X = x;
+			Y = y;
+			Z = z;
+			W = w;
+		}
+
+		public static Tuple Zero()
+		{
+			return new Tuple(0.0f, 0.0f, 0.0f, 0.0f);
+		}
+		public static Tuple Point(float x, float y, float z)
+		{
+			return new Tuple(x, y, z, 1.0f);
+		}
+
+
+		public static Tuple Vector(float x, float y, float z)
+		{
+			return new Tuple(x, y, z, 0.0f);
+		}
+
+		public static float Dot(Tuple left, Tuple right)
+		{
+			return (
+				left.X * right.X +
+				left.Y * right.Y +
+				left.Z * right.Z +
+				left.W * right.W);
+		}
+
+		/*
+		public static float Cross(Tuple left, Tuple right)
+		{
+
+		}
+		*/
+
+		public static Tuple operator +(Tuple left, Tuple right)
+		{
+			return new Tuple(
+				left.X + right.X,
+				left.Y + right.Y,
+				left.Z + right.Z,
+				left.W + right.W); //Math.Clamp(left.W + right.W, 0.0f, 1.0f));
+		}
+
+		public static Tuple operator -(Tuple left, Tuple right)
+		{
+			return new Tuple(
+				left.X - right.X,
+				left.Y - right.Y,
+				left.Z - right.Z,
+				left.W - right.W); //Math.Clamp(left.W - right.W, 0.0f, 1.0f));
+		}
+
+		// TASK: Should we be doing this, does it make sense to?
+		public static Tuple operator *(float scalar, Tuple tuple)
+		{
+			return tuple * scalar;
+		}
+		public static Tuple operator *(Tuple tuple, float scalar)
+		{
+			return new Tuple(
+				tuple.X * scalar,
+				tuple.Y * scalar,
+				tuple.Z * scalar,
+				tuple.W * scalar);
+		}
+
+
+		public static Tuple operator /(Tuple tuple, float scalar)
+		{
+			return new Tuple(
+				tuple.X / scalar,
+				tuple.Y / scalar,
+				tuple.Z / scalar,
+				tuple.W / scalar);
+		}
+
+		public static bool operator ==(Tuple left, Tuple right)
+		{
+			return (Math.Abs(left.X - right.X) < Epsilon) &&
+			       (Math.Abs(left.Y - right.Y) < Epsilon) &&
+			       (Math.Abs(left.Z - right.Z) < Epsilon) &&
+			       (Math.Abs(left.W - right.W) < Epsilon);
+		}
+
+		public static Tuple operator !(Tuple tuple)
+		{
+			return Zero() - tuple;
+		}
+		public static bool operator !=(Tuple left, Tuple right)
+		{
+			return !(left == right);
+		}
+
+		public bool Equals(Tuple other)
+		{
+			return (Math.Abs(X - other.X) < Epsilon) &&
+			       (Math.Abs(Y - other.Y) < Epsilon) &&
+			       (Math.Abs(Z - other.Z) < Epsilon) &&
+			       (Math.Abs(W - other.W) < Epsilon);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != this.GetType()) return false;
+			return Equals((Tuple) obj);
+		}
+
+		public float Magnitude()//Tuple tuple)
+		{
+			return (float) Math.Sqrt(
+				Math.Pow(X, 2) +
+				Math.Pow(Y, 2) +
+				Math.Pow(Z, 2) +
+				Math.Pow(W, 2));
+		}
+
+		public Tuple Normalize()
+		{
+			var magnitude = Magnitude();
+
+			return new Tuple(
+				X / magnitude,
+				Y / magnitude,
+				Z / magnitude,
+				W / magnitude);
+		}
+
+	}
+}
---
+++ RayTracerChallengeTest/Tuples.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tuple = RayTracerChallenge.Types.Tuple;
+
+
+namespace RayTracerChallengeTest
+{
+	[TestClass]
+	public class Tuples
+	{
+		[TestMethod]
+		public void Tuple_Is_Point()
+		{
+			// Arrange
+			var tuple = Tuple.Point(4.3f, -4.2f, 3.1f);
+			// Act
+
+			// Assert
+			Assert.IsFalse(tuple.IsVector);
+			Assert.IsTrue(tuple.IsPoint);
+		}
+
+		[TestMethod]
+		public void Tuple_Is_Vector()
+		{
+			// Arrange
+			var tuple = Tuple.Vector(4.3f, -4.2f, 3.1f);
+			// Act
+
+			// Assert
+			Assert.IsTrue(tuple.IsVector);
+			Assert.IsFalse(tuple.IsPoint);
+		}
+
+		[TestMethod]
+		public void Point_Has_W1()
+		{
+			// Arrange
+			var point = Tuple.Point(4.0f, -4.0f, 3.0f);
+			// Act
+
+			// Assert
+			Assert.AreEqual(1.0f, point.W);
+		}
+
+		[TestMethod]
+		public void Vector_Has_W0()
+		{
+			// Arrange
+			var vector = Tuple.Vector(4.0f, -4.0f, 3.0f);
+			// Act
+
+			// Assert
+			Assert.AreEqual(0.0f, vector.W);
+		}
+
+		[TestMethod]
+		public void Two_Vectors_Are_Equal()
+		{
+			// Arrange
+			var vectorOne = Tuple.Vector(4.0f, -4.0f, 3.0f);
+			var vectorTwo = Tuple.Vector(4.0f, -4.0f, 3.0f);
+			// Act
+
+			// Assert
+			Assert.AreEqual(vectorOne,vectorTwo);
+		}
+
+		[TestMethod]
+		public void Two_Points_Are_Equal()
+		{
+			// Arrange
+			var pointOne = Tuple.Point(4.0f, -4.0f, 3.0f);
+			var pointTwo = Tuple.Point(4.0f, -4.0f, 3.0f);
+			// Act
+
+			// Assert
+			Assert.AreEqual(pointOne, pointTwo);
+		}
+
+		[TestMethod]
+		public void Two_Points_Are_Not_Equal()
+		{
+			// Arrange
+			var pointOne = Tuple.Point(3.0f, 1.0f, 16.0f);
+			var pointTwo = Tuple.Point(4.0f, -4.0f, 3.0f);
+			// Act
+
+			// Assert
+			Assert.AreNotEqual(pointOne, pointTwo);
+		}
+
+		[TestMethod]
+		public void Two_Vectors_Are_Not_Equal()
+		{
+			// Arrange
+			var vectorOne = Tuple.Vector(3.0f, -1.0f, 16.0f);
+			var vectorTwo = Tuple.Vector(4.0f, -4.0f, 3.0f);
+			// Act
+
+			// Assert
+			Assert.AreNotEqual(vectorOne, vectorTwo);
+		}
+
+
+		[TestMethod]
+		public void Point_And_Vector_Are_Not_Equal_Given_Same_Values()
+		{
+			// Arrange
+			var vector = Tuple.Vector(4.0f, -4.0f, 3.0f);
+			var point = Tuple.Point(4.0f, -4.0f, 3.0f);
+			// Act
+
+			// Assert
+			Assert.AreNotEqual(vector, point);
+		}
+
+		[TestMethod]
+		public void Add_Two_Tuples()
+		{
+			// Arrange
+			var vector = Tuple.Vector(3.0f, -2.0f, 5.0f);
+			var point = Tuple.Point(-2.0f, 3.0f, 1.0f);
+			var expectedTuple = new Tuple(1.0f, 1.0f, 6.0f, 1.0f);
+			// Act
+			var newTuple = vector + point;
+			// Assert
+			Assert.AreEqual(expectedTuple, newTuple);
+		}
+
+		[TestMethod]
+		public void Subtract_Two_Points_Gives_Vector()
+		{
+			// Arrange
+			var pointOne = Tuple.Point(3.0f, 2.0f, 1.0f);
+			var pointTwo = Tuple.Point(5.0f, 6.0f, 7.0f);
+
+			var expectedVector = Tuple.Vector(-2.0f, -4.0f, -6.0f);
+			// Act
+			var vector = pointOne - pointTwo;
+			// Assert
+			Assert.AreEqual(expectedVector, vector);
+			Assert.IsTrue(vector.IsVector);
+		}
+
+		[TestMethod]
+		public void Subtract_Vector_From_Point_Gives_Point()
+		{
+			// Arrange
+			var point = Tuple.Point(3.0f, 2.0f, 1.0f);
+			var vector = Tuple.Vector(5.0f, 6.0f, 7.0f);
+
+			var expectedPoint = Tuple.Point(-2.0f, -4.0f, -6.0f);
+			// Act
+			var calculatedPoint = point - vector;
+			// Assert
+			Assert.AreEqual(expectedPoint, calculatedPoint);
+			Assert.IsTrue(calculatedPoint.IsPoint);
+		}
+
+		[TestMethod]
+		public void Subtract_Two_Vector_Gives_Vector()
+		{
+			// Arrange
+			var vectorOne = Tuple.Vector(3.0f, 2.0f, 1.0f);
+			var vectorTwo = Tuple.Vector(5.0f, 6.0f, 7.0f);
+
+			var expectedVector = Tuple.Vector(-2.0f, -4.0f, -6.0f);
+			// Act
+			var vector = vectorOne - vectorTwo;
+			// Assert
+			Assert.AreEqual(expectedVector, vector);
+			Assert.IsTrue(vector.IsVector);
+		}
+
+		[TestMethod]
+		public void Subtract_Vector_From_Zero_Negates_Vector()
+		{
+			// Arrange
+			var zero = Tuple.Zero();
+			var vector = Tuple.Vector(1.0f, -2.0f, 3.0f);
+
+			var expectedVector = Tuple.Vector(-1.0f, 2.0f, -3.0f);
+			// Act
+			var calculatedVector = zero - vector;
+			// Assert
+			Assert.AreEqual(expectedVector, calculatedVector);
+			Assert.IsTrue(calculatedVector.IsVector);
+		}
+
+		[TestMethod]
+		public void Negate_Tuple()
+		{
+			// Arrange
+			var tuple = new Tuple(1.0f, -2.0f, 3.0f, 4.0f);
+
+			var expectedTuple = new Tuple(-1.0f, 2.0f, -3.0f, -4.0f);
+			// Act
+			// Assert
+			Assert.AreEqual(expectedTuple, !tuple);
+		}
+
+		[TestMethod]
+		public void Multiply_Tuple_By_Scalar()
+		{
+			var tuple = new Tuple(1f, -2f, 3f, -4f);
+			var expectedTuple = new Tuple(3.5f, -7f, 10.5f, -14.0f);
+
+			var calculatedTuple = tuple * 3.5f;
+
+			Assert.AreEqual(expectedTuple, calculatedTuple);
+		}
+
+		[TestMethod]
+		public void Multiply_Tuple_By_Scalar_Fraction()
+		{
+			var tuple = new Tuple(1f, -2f, 3f, -4f);
+			var expectedTuple = new Tuple(0.5f, -1f, 1.5f, -2.0f);
+
+			var calculatedTuple = tuple * 0.5f;
+
+			Assert.AreEqual(expectedTuple, calculatedTuple);
+		}
+
+		[TestMethod]
+		public void Divide_Tuple_By_Scalar()
+		{
+			var tuple = new Tuple(1f, -2f, 3f, -4f);
+			var expectedTuple = new Tuple(0.5f, -1f, 1.5f, -2.0f);
+
+			var calculatedTuple = tuple / 2.0f;
+
+			Assert.AreEqual(expectedTuple, calculatedTuple);
+		}
+
+		[TestMethod]
+		public void Magnitude_X_Gives_One()
+		{
+			var vector = Tuple.Vector(1.0f, 0.0f, 0.0f);
+			Assert.AreEqual(1.0f, vector.Magnitude());
+		}
+
+		[TestMethod]
+		public void Magnitude_Y_Gives_One()
+		{
+			var vector = Tuple.Vector(0.0f, 1.0f, 0.0f);
+			Assert.AreEqual(1.0f, vector.Magnitude());
+		}
+
+		[TestMethod]
+		public void Magnitude_Z_Gives_One()
+		{
+			var vector = Tuple.Vector(0.0f, 0.0f, 1.0f);
+			Assert.AreEqual(1.0f, vector.Magnitude());
+		}
+
+		[TestMethod]
+		public void Magnitude_Positive_XYZ_Gives_Square_14()
+		{
+			var vector = Tuple.Vector(1.0f, 2.0f, 3.0f);
+			Assert.AreEqual((float)Math.Sqrt(14), vector.Magnitude());
+		}
+
+		[TestMethod]
+		public void Magnitude_Negative_XYZ_Gives_Square_14()
+		{
+			var vector = Tuple.Vector(-1.0f, -2.0f, -3.0f);
+			Assert.AreEqual((float)Math.Sqrt(14), vector.Magnitude());
+		}
+
+		[TestMethod]
+		public void Normalize_Vector_4_0_0_Gives_1_0_0()
+		{
+			var vector = Tuple.Vector(4.0f, 0.0f, 0.0f);
+
+			var expectedVectorUnit = Tuple.Vector(1.0f, 0.0f, 0.0f);
+
+			Assert.AreEqual(expectedVectorUnit, vector.Normalize());
+		}
+
+		[TestMethod]
+		public void Normalize_Vector_1_2_3()
+		{
+			var vector = Tuple.Vector(1.0f, 2.0f, 3.0f);
+			// ​												0.26726						 0.53452					 0.80178
+			var expectedVectorUnit = Tuple.Vector((float)(1/Math.Sqrt(14)), (float)(2 / Math.Sqrt(14)), (float)(3 / Math.Sqrt(14)));
+
+			Assert.AreEqual(expectedVectorUnit, vector.Normalize());
+		}
+
+		[TestMethod]
+		public void Mangnitude_Normalized()
+		{
+			var vector = Tuple.Vector(1.0f, 2.0f, 3.0f);
+			var normalizedVector = vector.Normalize();
+			Assert.IsTrue(normalizedVector.IsUnitVector);
+		}
+
+		[TestMethod]
+		public void Dot_Vectors()
+		{
+			var vectorOne = Tuple.Vector(1.0f, 2.0f, 3.0f);
+			var vectorTwo = Tuple.Vector(2.0f, 3.0f, 4.0f);
+
+			Assert.AreEqual(20.0f, Tuple.Dot(vectorOne, vectorTwo));
+		}
+	}
+}
